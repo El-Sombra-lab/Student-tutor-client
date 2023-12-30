87f63cea7645a27ccacf677850124d6ccf75a02c
Occxlnce office <110081105+El-Sombra-lab@users.noreply.github.com>
2023-12-30 17:53:33 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+import LoginView from './views/LoginView.vue';
+import StudentView from './views/StudentView.vue';
+export default{
+  components:{LoginView,StudentView}
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap');
+*{
+  font-family: 'Plus Jakarta Sans', sans-serif;
+}
+
+.flex-center{
+  @apply flex justify-center items-center;
+}
+</style>
